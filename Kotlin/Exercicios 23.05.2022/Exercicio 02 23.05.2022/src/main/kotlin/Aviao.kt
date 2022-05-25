ff45e19a529b0e007e1d2b74b7ebda2e966f04c7
unknown <lizandra.ferrari@hotmail.com>
2022-05-24 21:20:09 -0300

    gitgeneration

--- Kotlin/Exercicios 23.05.2022/Exercicio 02 23.05.2022/src/main/kotlin/Aviao.kt
+++ Kotlin/Exercicios 23.05.2022/Exercicio 02 23.05.2022/src/main/kotlin/Aviao.kt
@@ -6,16 +6,5 @@ class Aviao(
     var email:String,
     var comanda:Double) {
 
-    var cadastro = mutableListOf<String>()
-    fun cadastrar(cadastro: mutableListOf<String>) {
-        var cadastro = mutableListOf(
-        comanda to 0.0,
-        pessoa to "",
-        idade to 0,
-        show to "",
-        meet to "",
-        email to "")
 
-    }
 }
-
